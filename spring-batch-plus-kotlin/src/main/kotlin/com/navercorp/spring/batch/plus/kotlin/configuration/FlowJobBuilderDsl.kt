51e75d5d6c87228aa0931f33d571ce2016dbd44c
Taeik Lim <taeik.lim@navercorp.com>
2022-07-05 16:35:27 +0900

    Add Kotlin Dsl for FlowJob

---
+++ spring-batch-plus-kotlin/src/main/kotlin/com/navercorp/spring/batch/plus/kotlin/configuration/FlowJobBuilderDsl.kt
@@ -0,0 +1,35 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.kotlin.configuration
+
+import com.navercorp.spring.batch.plus.kotlin.configuration.support.BatchDslMarker
+import com.navercorp.spring.batch.plus.kotlin.configuration.support.DslContext
+import org.springframework.batch.core.job.builder.FlowJobBuilder
+import org.springframework.batch.core.job.builder.JobFlowBuilder
+
+/**
+ * A dsl for [FlowJobBuilder][org.springframework.batch.core.job.builder.FlowJobBuilder].
+ *
+ * @since 0.1.0
+ */
+@BatchDslMarker
+class FlowJobBuilderDsl internal constructor(
+    dslContext: DslContext,
+    flowJobBuilder: FlowJobBuilder
+) : FlowBuilderDsl<FlowJobBuilder>(dslContext, JobFlowBuilder(flowJobBuilder))
