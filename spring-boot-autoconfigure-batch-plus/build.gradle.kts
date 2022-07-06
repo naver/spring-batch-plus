af8d4ced3cd9fc6a91e039a3a0950de160c98e16
Taeik Lim <taeik.lim@navercorp.com>
2022-07-06 23:13:02 +0900

    Add BatchPlusAutoConfiguration

--- spring-boot-autoconfigure-batch-plus/build.gradle.kts
+++ spring-boot-autoconfigure-batch-plus/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("spring.batch.plus.java-library-conventions")
+    id("spring.batch.plus.kotlin-conventions")
     id("spring.batch.plus.maven-publish-conventions")
     id("spring.batch.plus.spring-conventions")
 }
@@ -9,7 +10,9 @@ dependencies {
     implementation(project(":spring-batch-plus-kotlin"))
 
     compileOnly("org.springframework.boot:spring-boot-autoconfigure")
+    compileOnly("org.springframework.batch:spring-batch-core")
 
-    testRuntimeOnly("org.springframework.boot:spring-boot-autoconfigure")
+    testImplementation("org.springframework.boot:spring-boot-autoconfigure")
     testImplementation("org.springframework.boot:spring-boot-test")
+    testImplementation("org.springframework.batch:spring-batch-core")
 }
---
+++ spring-boot-autoconfigure-batch-plus/src/test/kotlin/com/navercorp/spring/boot/autoconfiguer/batch/plus/BatchPlusAutoConfigurationTest.kt
@@ -0,0 +1,83 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.boot.autoconfiguer.batch.plus
+
+import com.navercorp.spring.batch.plus.kotlin.configuration.BatchDsl
+import com.navercorp.spring.boot.autoconfigure.batch.plus.BatchPlusAutoConfiguration
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.autoconfigure.AutoConfigurations
+import org.springframework.boot.test.context.assertj.AssertableApplicationContext
+import org.springframework.boot.test.context.runner.ApplicationContextRunner
+import org.springframework.context.annotation.Bean
+
+internal class BatchPlusAutoConfigurationTest {
+
+    private val contextRunner = ApplicationContextRunner()
+        .withConfiguration(AutoConfigurations.of(BatchPlusAutoConfiguration::class.java))
+
+    @Test
+    fun testRegister() {
+        contextRunner.withUserConfiguration(BatchConfiguration::class.java)
+            .run { context: AssertableApplicationContext? ->
+                assertThat(context).hasSingleBean(BatchDsl::class.java)
+            }
+    }
+
+    @Test
+    fun testNotRegisterWhenAlreadyRegistered() {
+        contextRunner.withUserConfiguration(BatchDslConfiguration::class.java)
+            .run { context: AssertableApplicationContext? ->
+                assertThat(context).hasSingleBean(BatchDsl::class.java)
+            }
+    }
+
+    @Test
+    fun testNotRegisterOnNoRequiredBeans() {
+        contextRunner.run { context: AssertableApplicationContext ->
+            assertThatThrownBy { context.getBean<BatchDsl>() }
+                .hasMessageContaining("No qualifying bean")
+        }
+    }
+
+    @EnableBatchProcessing
+    class BatchConfiguration
+
+    @EnableBatchProcessing
+    class BatchDslConfiguration {
+        @Bean
+        fun batchDsl(
+            beanFactory: BeanFactory?,
+            jobBuilderFactory: JobBuilderFactory?,
+            stepBuilderFactory: StepBuilderFactory?
+        ): BatchDsl {
+            return BatchDsl(
+                beanFactory!!,
+                jobBuilderFactory!!,
+                stepBuilderFactory!!
+            )
+        }
+    }
+}
