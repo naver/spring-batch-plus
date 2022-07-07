3a9212b2d2e661c81535ffb532b68173c3181c25
Taeik Lim <taeik.lim@navercorp.com>
2022-07-07 22:47:36 +0900

    Add delegate for ItemStreamWriter

---
+++ spring-batch-plus/src/main/java/com/navercorp/spring/batch/plus/item/ItemStreamWriterAdaptor.java
@@ -0,0 +1,60 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.item;
+
+import java.util.List;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamWriter;
+
+class ItemStreamWriterAdaptor<T> implements ItemStreamWriter<T> {
+
+	static <T> ItemStreamWriterAdaptor<T> withDelegate(ItemStreamWriterDelegate<T> delegate) {
+		return new ItemStreamWriterAdaptor<>(delegate);
+	}
+
+	private final ItemStreamWriterDelegate<T> delegate;
+
+	private ItemStreamWriterAdaptor(ItemStreamWriterDelegate<T> delegate) {
+		this.delegate = delegate;
+	}
+
+	@SuppressWarnings("NullableProblems")
+	@Override
+	public void open(ExecutionContext executionContext) {
+		this.delegate.onOpenWrite(executionContext);
+	}
+
+	@SuppressWarnings("NullableProblems")
+	@Override
+	public void write(List<? extends T> items) {
+		this.delegate.write(items);
+	}
+
+	@SuppressWarnings("NullableProblems")
+	@Override
+	public void update(ExecutionContext executionContext) {
+		this.delegate.onUpdateWrite(executionContext);
+	}
+
+	@Override
+	public void close() {
+		this.delegate.onCloseWrite();
+	}
+}
---
+++ spring-batch-plus/src/test/java/com/navercorp/spring/batch/plus/item/ItemStreamWriterAdaptorTest.java
@@ -0,0 +1,167 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.item;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.item.ExecutionContext;
+
+class ItemStreamWriterAdaptorTest {
+
+	@Test
+	void testOpen() {
+		// given
+		AtomicInteger onOpenWriteCallCount = new AtomicInteger();
+		ItemStreamWriterAdaptor<Integer> itemStreamWriterAdaptor = ItemStreamWriterAdaptor.withDelegate(
+			new ItemStreamWriterDelegate<Integer>() {
+				@Override
+				public void onOpenWrite(ExecutionContext executionContext) {
+					onOpenWriteCallCount.incrementAndGet();
+				}
+
+				@Override
+				public void write(List<? extends Integer> items) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void onUpdateWrite(ExecutionContext executionContext) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void onCloseWrite() {
+					throw new UnsupportedOperationException();
+				}
+			});
+
+		// when
+		itemStreamWriterAdaptor.open(new ExecutionContext());
+
+		// then
+		assertThat(onOpenWriteCallCount.get()).isEqualTo(1);
+	}
+
+	@Test
+	void testWrite() {
+		// given
+		AtomicInteger writeCallCount = new AtomicInteger();
+		ItemStreamWriterAdaptor<Integer> itemStreamWriterAdaptor = ItemStreamWriterAdaptor.withDelegate(
+			new ItemStreamWriterDelegate<Integer>() {
+				@Override
+				public void onOpenWrite(ExecutionContext executionContext) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void write(List<? extends Integer> items) {
+					writeCallCount.incrementAndGet();
+				}
+
+				@Override
+				public void onUpdateWrite(ExecutionContext executionContext) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void onCloseWrite() {
+					throw new UnsupportedOperationException();
+				}
+			});
+
+		// when
+		itemStreamWriterAdaptor.write(Collections.emptyList());
+
+		// then
+		assertThat(writeCallCount.get()).isEqualTo(1);
+	}
+
+	@Test
+	void testUpdate() {
+		// given
+		AtomicInteger onUpdateWriteCallCount = new AtomicInteger();
+		ItemStreamWriterAdaptor<Integer> itemStreamWriterAdaptor = ItemStreamWriterAdaptor.withDelegate(
+			new ItemStreamWriterDelegate<Integer>() {
+				@Override
+				public void onOpenWrite(ExecutionContext executionContext) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void write(List<? extends Integer> items) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void onUpdateWrite(ExecutionContext executionContext) {
+					onUpdateWriteCallCount.incrementAndGet();
+				}
+
+				@Override
+				public void onCloseWrite() {
+					throw new UnsupportedOperationException();
+				}
+			});
+
+		// when
+		itemStreamWriterAdaptor.update(new ExecutionContext());
+
+		// then
+		assertThat(onUpdateWriteCallCount.get()).isEqualTo(1);
+	}
+
+	@Test
+	void testClose() {
+		// given
+		AtomicInteger onCloseCallCount = new AtomicInteger();
+		ItemStreamWriterAdaptor<Integer> itemStreamWriterAdaptor = ItemStreamWriterAdaptor.withDelegate(
+			new ItemStreamWriterDelegate<Integer>() {
+				@Override
+				public void onOpenWrite(ExecutionContext executionContext) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void write(List<? extends Integer> items) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void onUpdateWrite(ExecutionContext executionContext) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void onCloseWrite() {
+					onCloseCallCount.incrementAndGet();
+				}
+			});
+
+		// when
+		itemStreamWriterAdaptor.close();
+
+		// then
+		assertThat(onCloseCallCount.get()).isEqualTo(1);
+	}
+}
