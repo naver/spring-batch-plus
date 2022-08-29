e73bdcaaba1cc9942428221c174edc72c2b2a6a2
Taeik Lim <taeik.lim@navercorp.com>
2022-08-29 16:54:12 +0900

    Add ClearRunIdIncrementer comparison sample

---
+++ spring-batch-plus-sample/spring-batch-plus-clear-run-id-incrementer/src/main/java/com/navecorp/spring/batch/plus/sample/comparison/bad/TestJobConfig.java
@@ -0,0 +1,72 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navecorp.spring.batch.plus.sample.comparison.bad;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestJobConfig {
+
+	@Bean
+	public Job testJob(
+		JobBuilderFactory jobBuilderFactory,
+		StepBuilderFactory stepBuilderFactory
+	) {
+		return jobBuilderFactory.get("testJob")
+			.incrementer(new RunIdIncrementer())
+			.start(
+				stepBuilderFactory.get("testStep")
+					.tasklet(testTasklet(null, null))
+					.build()
+			)
+			.build();
+	}
+
+	@StepScope
+	@Bean
+	public Tasklet testTasklet(
+		@Value("#{jobParameters['longValue']}") Long longValue,
+		@Value("#{jobParameters['stringValue']}") String stringValue
+	) {
+		return (contribution, chunkContext) -> {
+			Long result;
+			if (stringValue != null) {
+				result = longValue + Long.parseLong(stringValue);
+			} else {
+				result = 999L;
+			}
+
+			ExecutionContext jobExecutionContext = contribution.getStepExecution()
+				.getJobExecution()
+				.getExecutionContext();
+			jobExecutionContext.putLong("result", result);
+			return RepeatStatus.FINISHED;
+		};
+	}
+}
