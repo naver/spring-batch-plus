c8d459dbbe2027d93a0631838178392b6b8e48b6
Taeik Lim <sibera21@gmail.com>
2022-11-01 00:03:50 +0900

    Apply github actions for ci

--- buildSrc/src/main/kotlin/spring.batch.plus.coverage-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/spring.batch.plus.coverage-conventions.gradle.kts
@@ -10,27 +10,12 @@ kover {
     engine.set(DefaultIntellijEngine) // use IntellijEngine to cover all kotlin
 
     xmlReport {
-        onCheck.set(false)
+        onCheck.set(true)
+        reportFile.set(layout.buildDirectory.file("kover/result.xml"))
     }
 
     htmlReport {
         onCheck.set(true)
         reportDir.set(layout.buildDirectory.dir("kover/html"))
-    }
-
-    verify {
-        onCheck.set(true)
-        rule {
-            isEnabled = true
-            name = "Test Coverage Rule" // custom name for the rule
-            target = kotlinx.kover.api.VerificationTarget.ALL // specify by which entity the code for separate coverage evaluation will be grouped
-
-            bound {
-                minValue = 90
-                maxValue = 100
-                counter = kotlinx.kover.api.CounterType.INSTRUCTION // (LINE, INSTRUCTION, BRANCH)
-                valueType = kotlinx.kover.api.VerificationValueType.COVERED_PERCENTAGE // (COVERED_COUNT, MISSED_COUNT, COVERED_PERCENTAGE, MISSED_PERCENTAGE)
-            }
-        }
     }
 }
