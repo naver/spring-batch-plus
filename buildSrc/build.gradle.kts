fac6c96256f5271c8f72a861d8cdc6e87d225ce9
Taeik Lim <sibera21@gmail.com>
2023-02-13 03:19:50 +0900

    Remove unused config in buildSrc
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,15 +8,6 @@ repositories {
     gradlePluginPortal() // give accees to gradle community plugins
 }
 
-// to remove following warning
-// Task :buildSrc:compileKotlin
-// 'compileJava' task (current target is 17) and 'compileKotlin' task (current target is 1.8) jvm target compatibility should be set to the same Java version.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.3.0")
