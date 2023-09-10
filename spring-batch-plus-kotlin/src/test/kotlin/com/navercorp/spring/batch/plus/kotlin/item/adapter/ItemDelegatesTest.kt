ef06fc97b306fc15363741ed262e3a5bb66d0b65
Taeik Lim <sibera21@gmail.com>
2023-09-10 23:41:37 +0900

    Polish ItemDelegateTest
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- spring-batch-plus-kotlin/src/test/kotlin/com/navercorp/spring/batch/plus/kotlin/item/adapter/ItemDelegatesTest.kt
+++ spring-batch-plus-kotlin/src/test/kotlin/com/navercorp/spring/batch/plus/kotlin/item/adapter/ItemDelegatesTest.kt
@@ -18,132 +18,43 @@
 
 package com.navercorp.spring.batch.plus.kotlin.item.adapter
 
+import com.navercorp.spring.batch.plus.item.adapter.ItemProcessorAdapter
 import com.navercorp.spring.batch.plus.item.adapter.ItemStreamReaderProcessorWriter
-import com.navercorp.spring.batch.plus.kotlin.configuration.BatchDsl
+import com.navercorp.spring.batch.plus.item.adapter.ItemStreamWriterAdapter
+import com.navercorp.spring.batch.plus.item.adapter.StepScopeItemStreamReader
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.batch.core.BatchStatus
-import org.springframework.batch.core.JobParametersBuilder
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
-import org.springframework.batch.core.configuration.annotation.StepScope
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.item.Chunk
 import org.springframework.batch.item.ExecutionContext
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager
-import org.springframework.beans.factory.BeanFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.beans.factory.getBean
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.datasource.DataSourceTransactionManager
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
-import org.springframework.transaction.TransactionManager
 import reactor.core.publisher.Flux
-import javax.sql.DataSource
 
 internal class ItemDelegatesTest {
 
-    companion object {
-        private var processCallCount = 0
-        private var writeCallCount = 0
-    }
-
     @Test
     fun testExtensions() {
-        // given
-        val context = AnnotationConfigApplicationContext(TestConfiguration::class.java)
-        val jobLauncher = context.getBean<JobLauncher>()
-        val batch = context.getBean<BatchDsl>()
-        val testTasklet = context.getBean<TestClass>("testTasklet")
-
         // when
-        val job = batch {
-            job("testJob") {
-                step("testStep") {
-                    chunk<Int, String>(3, ResourcelessTransactionManager()) {
-                        reader(testTasklet.asItemStreamReader())
-                        processor(testTasklet.asItemProcessor())
-                        writer(testTasklet.asItemStreamWriter())
-                    }
-                }
-            }
-        }
-        val jobParameters = JobParametersBuilder()
-            .addLong("limit", 20L)
-            .toJobParameters()
-        val jobExecution = jobLauncher.run(job, jobParameters)
+        val testClass = TestClass()
+        val itemStreamReader = testClass.asItemStreamReader()
+        val itemProcessor = testClass.asItemProcessor()
+        val itemStreamWriter = testClass.asItemStreamWriter()
 
         // then
-        assertThat(jobExecution.status).isEqualTo(BatchStatus.COMPLETED)
-        assertThat(processCallCount).isEqualTo(20)
-        assertThat(writeCallCount).isEqualTo(7) // ceil(20/3)
+        assertThat(itemStreamReader).isInstanceOf(StepScopeItemStreamReader::class.java)
+        assertThat(itemProcessor).isInstanceOf(ItemProcessorAdapter::class.java)
+        assertThat(itemStreamWriter).isInstanceOf(ItemStreamWriterAdapter::class.java)
     }
 
-    internal open class TestClass(
-        private val limit: Long,
-    ) : ItemStreamReaderProcessorWriter<Int, String> {
+    internal open class TestClass : ItemStreamReaderProcessorWriter<Int, String> {
 
         override fun readFlux(executionContext: ExecutionContext): Flux<Int> {
-            var count = 0
-            return Flux.generate { sink ->
-                if (count < limit) {
-                    sink.next(count)
-                    ++count
-                } else {
-                    sink.complete()
-                }
-            }
+            return Flux.empty()
         }
 
         override fun process(item: Int): String {
-            ++processCallCount
             return item.toString()
         }
 
         override fun write(chunk: Chunk<out String>) {
-            ++writeCallCount
-        }
-    }
-
-    @Configuration
-    @EnableBatchProcessing(
-        dataSourceRef = "metadataDataSource",
-        transactionManagerRef = "metadataTransactionManager",
-    )
-    private open class TestConfiguration {
-
-        @Bean
-        open fun batchDsl(
-            beanFactory: BeanFactory,
-            jobRepository: JobRepository,
-        ): BatchDsl = BatchDsl(
-            beanFactory,
-            jobRepository,
-        )
-
-        @Bean
-        open fun metadataTransactionManager(): TransactionManager {
-            return DataSourceTransactionManager(metadataDataSource())
-        }
-
-        @Bean
-        open fun metadataDataSource(): DataSource {
-            return EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("/org/springframework/batch/core/schema-h2.sql")
-                .generateUniqueName(true)
-                .build()
-        }
-
-        @Bean
-        @StepScope
-        open fun testTasklet(
-            @Value("#{jobParameters['limit']}") limit: Long,
-        ): TestClass {
-            return TestClass(limit)
         }
     }
 }
