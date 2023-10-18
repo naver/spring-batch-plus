56579a2a3df153eada968fe65493ee9cd9586a02
Taeik Lim <sibera21@gmail.com>
2023-10-18 18:20:11 +0900

    Bump up kotlin build version (1.7.10 -> 1.9.10)
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.3.0")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
 }
