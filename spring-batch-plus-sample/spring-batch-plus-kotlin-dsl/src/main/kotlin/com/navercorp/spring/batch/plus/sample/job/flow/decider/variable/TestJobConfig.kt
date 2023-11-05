198bc216a7290cf5e02c582b601827f35aed95ac
Taeik Lim <sibera21@gmail.com>
2023-11-05 22:32:39 +0900

    Reorganize flow samples
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

---
+++ spring-batch-plus-sample/spring-batch-plus-kotlin-dsl/src/main/kotlin/com/navercorp/spring/batch/plus/sample/job/flow/decider/variable/TestJobConfig.kt
@@ -0,0 +1,59 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.sample.job.flow.decider.variable
+
+import com.navercorp.spring.batch.plus.kotlin.configuration.BatchDsl
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.job.flow.FlowExecutionStatus
+import org.springframework.batch.core.job.flow.JobExecutionDecider
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+
+@Configuration
+open class TestJobConfig(
+    private val batch: BatchDsl,
+    private val transactionManager: PlatformTransactionManager,
+) {
+
+    @Bean
+    open fun testJob(): Job = batch {
+        job("testJob") {
+            decider(testDecider()) {
+                on("COMPLETED") {
+                    end()
+                }
+                on("FAILED") {
+                    step("transitionStep") {
+                        tasklet({ _, _ -> RepeatStatus.FINISHED }, transactionManager)
+                    }
+                }
+                on("*") {
+                    stop()
+                }
+            }
+        }
+    }
+
+    @Bean
+    open fun testDecider(): JobExecutionDecider = JobExecutionDecider { _, _ ->
+        FlowExecutionStatus.FAILED
+    }
+}
