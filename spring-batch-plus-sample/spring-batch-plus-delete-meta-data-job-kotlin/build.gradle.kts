1bc00b2457fcf14feed7cdf23063e3f9106defaa
Taeik Lim <sibera21@gmail.com>
2024-07-28 23:31:42 +0900

    Add test dependencies to samples
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- spring-batch-plus-sample/spring-batch-plus-delete-meta-data-job-kotlin/build.gradle.kts
+++ spring-batch-plus-sample/spring-batch-plus-delete-meta-data-job-kotlin/build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("org.jlleitschuh.gradle.ktlint")
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 repositories {
@@ -25,9 +26,22 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     }
 }
 
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    maxParallelForks = Runtime.getRuntime().availableProcessors()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.0")
+    }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-batch:3.2.0")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-web") // to check metadata in h2
     implementation(project(":spring-boot-starter-batch-plus-kotlin"))
     runtimeOnly("com.h2database:h2:2.1.214")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
