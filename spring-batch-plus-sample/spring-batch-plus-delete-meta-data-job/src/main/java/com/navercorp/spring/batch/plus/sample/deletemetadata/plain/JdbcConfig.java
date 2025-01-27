f48725dd9e07c4a975e4069bfcd3ea5cec930859
Taeik Lim <sibera21@gmail.com>
2025-01-27 16:31:13 +0900

    Fix wrong package name (com.navecorp -> com.navercorp)
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

---
+++ spring-batch-plus-sample/spring-batch-plus-delete-meta-data-job/src/main/java/com/navercorp/spring/batch/plus/sample/deletemetadata/plain/JdbcConfig.java
@@ -0,0 +1,47 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.sample.deletemetadata.plain;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.batch.BatchDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class JdbcConfig {
+
+	/*
+		localhost:8080/h2-console
+			jdbc url : jdbc:h2:mem:jobdb
+			user : "sa"
+			pw   : ""
+	 */
+	@BatchDataSource
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setName("jobdb")
+			.setType(EmbeddedDatabaseType.H2)
+			.ignoreFailedDrops(true)
+			.build();
+	}
+}
