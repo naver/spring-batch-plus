fdfe0a2775f5d3298a9a958b4116d1ce6c8bd324
Taeik Lim <sibera21@gmail.com>
2025-02-02 19:55:14 +0900

    Add sample for reader-processor
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

---
+++ spring-batch-plus-sample/single-class-reader-processor-writer-sample/src/test/java/com/navercorp/spring/batch/plus/sample/iterator/readerprocessor/SampleTasklet.java
@@ -0,0 +1,61 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.sample.iterator.readerprocessor;
+
+import java.util.Iterator;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import com.navercorp.spring.batch.plus.step.adapter.ItemStreamIteratorReaderProcessor;
+
+@Component
+@StepScope
+class SampleTasklet implements ItemStreamIteratorReaderProcessor<Integer, String> {
+
+	@Value("#{jobParameters['totalCount']}")
+	private long totalCount;
+
+	private int count = 0;
+
+	@NonNull
+	@Override
+	public Iterator<? extends Integer> readIterator(@NonNull ExecutionContext executionContext) {
+		System.out.println("totalCount: " + totalCount);
+		return new Iterator<>() {
+			@Override
+			public boolean hasNext() {
+				return count < totalCount;
+			}
+
+			@Override
+			public Integer next() {
+				return count++;
+			}
+		};
+	}
+
+	@Override
+	public String process(@NonNull Integer item) {
+		return "'" + item.toString() + "'";
+	}
+}
---
+++ spring-batch-plus-sample/single-class-reader-processor-writer-sample/src/test/java/com/navercorp/spring/batch/plus/sample/simple/readerprocessor/SampleTasklet.java
@@ -0,0 +1,50 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.sample.simple.readerprocessor;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import com.navercorp.spring.batch.plus.step.adapter.ItemStreamSimpleReaderProcessor;
+
+@Component
+@StepScope
+class SampleTasklet implements ItemStreamSimpleReaderProcessor<Integer, String> {
+
+	@Value("#{jobParameters['totalCount']}")
+	private long totalCount;
+
+	private int count = 0;
+
+	@Override
+	public Integer read() {
+		if (count < totalCount) {
+			return count++;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public String process(@NonNull Integer item) {
+		return "'" + item.toString() + "'";
+	}
+}
