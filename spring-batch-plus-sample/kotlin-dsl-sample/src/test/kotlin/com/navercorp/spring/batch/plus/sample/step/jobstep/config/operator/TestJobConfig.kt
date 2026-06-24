32d8ef2f4e5c81206c761126b5b81a70c9961e3a
Taeik Lim <sibera21@gmail.com>
2026-06-24 21:05:46 +0900

    Add JobStepBuilderDsl.operator sample
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

---
+++ spring-batch-plus-sample/kotlin-dsl-sample/src/test/kotlin/com/navercorp/spring/batch/plus/sample/step/jobstep/config/operator/TestJobConfig.kt
@@ -0,0 +1,66 @@
+/*
+ * Spring Batch Plus
+ *
+ * Copyright 2022-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.spring.batch.plus.sample.step.jobstep.config.operator
+
+import com.navercorp.spring.batch.plus.kotlin.configuration.BatchDsl
+import org.springframework.batch.core.job.Job
+import org.springframework.batch.core.job.JobExecution
+import org.springframework.batch.core.job.parameters.JobParameters
+import org.springframework.batch.core.launch.JobOperator
+import org.springframework.batch.infrastructure.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+
+@Configuration
+open class TestJobConfig(
+    private val batch: BatchDsl,
+    private val transactionManager: PlatformTransactionManager,
+    private val jobOperator: JobOperator,
+) {
+    @Bean
+    open fun testJob(): Job =
+        batch {
+            job("testJob") {
+                step("testStep") {
+                    job(anotherJob()) {
+                        operator(loggingJobOperator(jobOperator))
+                    }
+                }
+            }
+        }
+
+    private fun loggingJobOperator(delegate: JobOperator): JobOperator =
+        object : JobOperator by delegate {
+            override fun start(job: Job, jobParameters: JobParameters): JobExecution {
+                println("launch ${job.name}!!!")
+                return delegate.start(job, jobParameters)
+            }
+        }
+
+    @Bean
+    open fun anotherJob() =
+        batch {
+            job("anotherJob") {
+                step("anotherJobStep") {
+                    tasklet({ _, _ -> RepeatStatus.FINISHED }, transactionManager)
+                }
+            }
+        }
+}
